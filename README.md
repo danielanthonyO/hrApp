0dd0985c33402e8854aaa3e0ac75a1f2473fc7b2
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-05-26 10:16:54 +0300

    Setting up react repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# Week 1
+
+This project is a simple React application bootstrapped with Vite.
+It demonstrates the use of React components, CSS styling, and modern JavaScript features.
+
+## Features
+
+- **React Components**: Includes a `Box` component and an `App` component in `main.jsx` that dynamically render content.
+- **CSS Styling**: Custom styles are applied using `App.css` and `index.css`.
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const name = "Daniel Anthony";
+const first_page = 1998;
+
+const Box = () => {
+  return <p>Greetings from Box component</p>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, my name is {name} </h1>
+      <p>I have coding {2025 - first_page} years </p>
+      <Box />
+    </div>
+  );
+};
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
