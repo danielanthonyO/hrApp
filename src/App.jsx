a3fb2c993a07377cad5697e9c3f6564042df8a3c
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-05-26 10:50:45 +0300

    Initial commit: create base React app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
 import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Box from "./Box";
 
 const App = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <Header name="Daniel Olugbenga" />
+      <main>
+      <h1>HR REACT DEMO APP</h1>
+    <div className="boxes">
+    <Box
+      fullName="Chinedu Okafor"
+      title="Software Engineer"
+      age="30"
+      myAnimal="dog"
+    />
+    <Box
+      fullName="Aisha Bello"
+      title="Product Manager"
+      age="45"
+      myAnimal="bear"
+    />
+    <Box
+      fullName="Tunde Bakare"
+      title="Web Designer"
+      age="32"
+      myAnimal="wolf"
+    />
+    <Box
+      fullName="Ngozi Umeh"
+      title="Software Developer"
+      age="38"
+      myAnimal="cat"
+    />
+    <Box
+      fullName="Babatunde Ajayi"
+      title="Product Owner"
+      age="29"
+      myAnimal="deer"
+    />
+    <Box
+      fullName="Funmi Adeyemi"
+      title="Software Architect"
+      age="41"
+      myAnimal="dog"
+    />
+  </div>
+</main>
+      <Footer year={2025} />
     </>
   );
 };
 
 export default App;
+
